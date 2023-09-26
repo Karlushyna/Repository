c667c1d9ca9683eeecae505fb5b281b3683992c0
Karlushyna Lesya <karlushyna.l@gmail.com>
2023-09-26 11:57:20 -0400

    upd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes, FaGithub, FaLinkedin, FaTelegramPlane,  } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/pagelogo.png';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
